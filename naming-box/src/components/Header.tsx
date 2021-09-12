c8ff274f97eb8714eb848b14e61a50612b5ebf59
Hanseok <hasuikhs@gmail.com>
2021-09-12 23:06:19 +0900

    210912|Update navbar

--- naming-box/src/components/Header.tsx
+++ naming-box/src/components/Header.tsx
@@ -3,19 +3,21 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import '../components/Header.css';
 
 export default function Header(): JSX.Element {
-    return (
-        <div>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">Naming Box</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Projects</Nav.Link>
-                        <Nav.Link href="#pricing">Words</Nav.Link>
-                        <Nav.Link href="#login" className="fr">LogIn</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Naming Box</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Projects</Nav.Link>
+            <Nav.Link href="#pricing">Words</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#pricing">LogIn</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
